947818c33c95583796d36c97981ec830bac31766
Marten Nascimento Maia <marten.maia@ilog.com.br>
2022-12-07 22:39:07 -0300

    Inclusao do modelmapper

--- LabSchool_Spring/src/main/java/tech/devinhouse/labschool_spring/LabSchoolSpringApplication.java
+++ LabSchool_Spring/src/main/java/tech/devinhouse/labschool_spring/LabSchoolSpringApplication.java
@@ -1,7 +1,10 @@
 package tech.devinhouse.labschool_spring;
 
+import org.modelmapper.ModelMapper;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.validation.beanvalidation.LocalValidatorFactoryBean;
 
 @SpringBootApplication
 public class LabSchoolSpringApplication {
@@ -9,5 +12,12 @@ public class LabSchoolSpringApplication {
     public static void main(String[] args) {
         SpringApplication.run(LabSchoolSpringApplication.class, args);
     }
+
+    @Bean
+    public ModelMapper modelMapper() {
+        ModelMapper modelMapper = new ModelMapper();
+        return modelMapper;
+    }
+
 
 }
